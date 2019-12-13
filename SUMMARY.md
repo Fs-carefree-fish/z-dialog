4590369c7eb4f8de4104b105cd579d9dedf1ed14
张铮 <723136370@qq.com>
2019-12-13 10:49:20 +0800

    初始化项目

---
+++ SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+* [项目介绍](README.md)
+* [使用文档](doc/use/README.md)
+    * [使用](doc/use/use.md)
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert("z-dialog")
+var z_dialog = function () {
+  this.a = 'A'
+}
+
+window.z_dialog = z_dialog;
